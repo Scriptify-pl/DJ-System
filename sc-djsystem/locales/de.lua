e15a38999f8c8634ba9786575f4aabeca1fd2bf5
Blutwurst <faid@live.de>
2023-04-13 21:02:38 +0200

    added german translation

---
+++ sc-djsystem/locales/de.lua
@@ -0,0 +1,9 @@
+Locales['de'] = {
+    ['dj_text_1'] = 'Gib die Song URL ein',
+    ['dj_text_2'] = 'Lautstärke eingeben (0.0, 1.0)',
+    ['dj_text_3'] = 'Hördistanz eingeben (0.0, 50.0)',
+    ['dj_notify_1'] = 'Das DJ Menü wurde geschlossen.',
+    ['dj_notify_2'] = 'Nicht korrekt ausgefüllt!',
+    ['dj_notify_3'] = 'Das kannst du nicht tun!',
+    ['dj_showTextUI'] = '[E] - DJ Menü öffnen',
+}
